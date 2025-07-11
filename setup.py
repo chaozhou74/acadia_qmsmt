baa01122b123dce0c89e7bd28e7c99e5a8e8c14d
Nico <nico.zani@yale.edu>
2025-07-11 12:28:05 -0400

    fixed setup to make all packages accessible

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
       name='acadia_qmsmt',
       version='0.0.1',
-      packages=['acadia_qmsmt'],
+      packages=find_packages(),
       
       extras_require={
         "host": [
