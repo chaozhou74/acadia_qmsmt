e5d6e5781e3e553557ffb25b983a7adda075b196
Chao <chao.zhou.cz445@yale.edu>
2025-07-11 19:10:26 -0400

    updated `setup.py` to use `setuptools` so modules can be found automatically when install with normal pip install

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-      name='acadia_qmsmt',
-      version='0.0.1',
-      packages=['acadia_qmsmt'],
-      
-      extras_require={
+    name='acadia_qmsmt',
+    version='0.0.1',
+    packages=find_packages(),  # Auto-detects all submodules and subpackages
+    extras_require={
         "host": [
             "ruamel.yaml"
         ],
